6e6b645ab6dd2990b37def24772f127b88eefe64
vaishnavi0701 <90175670+vaishnavi0701@users.noreply.github.com>
2022-04-26 22:43:30 +0530

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,497 @@
+// const n = 5;
+// // looping from i = 1 to 5
+// for (let i = 1; i <= n; i++) {
+//     console.log(`I love JavaScript.`);
+// }
+
+
+// const n = 5;
+// for (let i = 1; i <= n; i++) {
+//     console.log(i);    
+// }
+
+// var a=3
+// for (var i=1; i<=a;i++){
+//     console.log(i)
+// }
+
+// var i=1
+// while (i<=100){
+//    console.log(i)
+//    i=i+1
+// }
+
+
+
+// var i=1
+// while (i<=100){
+//    if (i%7==0){
+//       console.log(i)
+      
+//    }i=i+1
+// }
+
+
+
+// var count = 1;
+// while (count<=10) {
+//    console.log(count);
+//    count +=1;
+// }
+
+
+// var i=1
+// while (i<=10){
+//     if (i%2==0){
+//         console.log(i)
+//     }
+//     i=i+1
+// }
+
+
+// var a=require("readline-sync")
+// var b=a.question("enter num : ")
+// var i=1
+// while (i<=10){
+//    console.log(b,"*",i,"=",b*i)
+//    i=i+1
+// }
+
+// var a=require("readline-sync")
+// var b=a.question("enter num : ")
+// var c=a.question("enter num : ")
+// var i=0
+// while (b>0){
+//    console.log(b*c)
+//    i=i+1
+// }
+
+
+// var a=require("readline-sync")
+// var b=a.question("enter year : ")
+// var i=b
+// while (i<=b){
+//    if (i%4==0 && i%400==0){
+//       console.log(i,"leap year")
+//    }
+//    else{
+//       console.log(i,"not leap year")
+//    }
+//    i=i+1
+// }
+
+
+
+// var z=require("readline-sync")
+// var num=z.question("enter num : ")
+// var i=1
+// var a=0
+// while (i<=num){
+//    if (num%i==0){
+//       a=a+1
+//    }i=i+1
+// }
+// if (a==2){
+//    console.log(num,"prime")
+// }
+// else{
+//    console.log(num,"not prime")
+// }
+
+
+
+// var z=require("readline-sync")
+// var num=z.questionInt("enter num : ")
+// i=1
+// while (num<=1){
+//    i=i+1
+// }
+// console.log(num+1)
+
+
+
+// var z=require("readline-sync")
+// var num=z.questionInt("enter num : ")
+// i=1
+// a=0
+// while (i<num){
+//    if (num%i==0){
+//       a=a+i
+//    }i=i+1
+// }
+// if (a==num){
+//    console.log(num,"perfect num")
+// }
+// else{
+//    console.log(num,"not perfect")
+// }
+
+
+// var z=require("readline-sync")
+// var num=z.questionInt("enter num : ")
+// var i=0
+// while (num>0){
+//    i=(i*10)+num%10
+//    num=Math.floor(n/10)
+// console.log(i)
+// i=i+1
+// }
+
+
+// var z=require("readline-sync")
+// var num=z.questionInt("enter num : ")
+// var temp=number
+// while (temp>0){
+//       var rem=temp%10
+//       sum=sum+rem*rem*rem
+//       temp=parseInt(temp/10)
+// if (sum==number){
+//    console.log(`${number} is amstrong`)
+   
+// }
+// else{
+//    console.log(`${number} is not amstrong `)
+// }
+
+
+
+// var z=require("readline-sync")
+// var num=z.questionInt("enter num : ")
+// var num=n
+// var sum=0
+// while (n>0){
+//    sum=sum+(n%10)
+//    n=Math.floor(n/10)
+// console.log(sum)
+// }
+// if (num%sum==0){
+//    console.log(sum,"harshad n0")
+// }
+// else{
+//    console.log(sum,"not harshad no")
+// }
+
+
+// var i=1
+// var sum=0
+// while (i<=10){
+   
+//    var z=require("readline-sync")
+//    var x=z.questionInt("enter num : ")
+//    sum=sum+x
+//    i=i+1
+//    console.log(sum)
+// }
+
+
+   // var z=require("readline-sync")
+   // var num=z.questionInt("enter num : ")
+   // var fact=1
+   // var i=1
+   // while (i<=num){
+   //    fact=fact*i
+   //    i=i+1
+   // console.log(fact,"factorial")
+   // }
+
+
+
+   // var z=require("readline-sync")
+   // var num=z.questionInt("enter num : ")
+   // var i=0
+   // while (i<num){
+   //    a=num%10
+   //    i=i+1
+   // console.log(a)
+   // }
+
+
+
+// var i=1
+// while (i<=100){
+//    if (i%2==0){
+//       console.log(i)
+//    }
+//    else{
+//       console.log(-i)
+   
+//    }
+//    i++
+// }
+
+
+// var i=1
+// do {
+//    if (i%2==0){
+//       console.log(i)
+//    }
+//    else{
+//       console.log(-i)
+//    }
+//    i=i+1
+// }
+// while (i<=10)
+
+
+// var z=require("readline-sync")
+// var num=z.questionInt("enter num : ")
+// var fact=1
+// var i=1
+// do{
+//    fact=fact*i
+//    i=i+1
+//    console.log(fact,"factorial")
+// }
+// while(i<=num)
+
+
+
+
+
+
+// var z=require("readline-sync")
+// var num=z.questionInt("enter num : ")
+// let temp=number;
+// while (temp>0){
+//       var rem=temp%10
+//       sum=sum+rem*rem*rem
+//       temp=parseInt(temp/10)
+// if (sum==number){
+//    console.log(`${number} is amstrong`)
+// }
+// else{
+//    console.log(`${number} is not amstrong `)
+// }}
+
+
+
+// var n=require("readline-sync");
+// var name=n.question("enter name: ")
+// var store=name
+// var string=""
+// for (var i=name.length-1;i>=0;i--) {
+//    string=string+name[i]
+// }
+// console.log(string)
+// if (store===string) {
+//    console.log("its palindrome string")
+// }
+// else {
+//    console.log("it's not a palindrome string")
+// }
+
+
+
+// var z=require("readline-sync")
+// var num=z.question("enter num : ")
+// var i=1
+// var a=0
+// while (i<=num){
+//    if(num%i==0){
+//       a=a+1
+   
+//    }
+//    i=i+1
+// }
+// if(a==2){
+//    console.log(num,"prime")
+// }
+// else{
+//    console.log(num,"not prime")
+// }
+
+
+// var z=require("readline-sync")
+// var num=z.question("enter num : ")
+// var i=0
+// while(i<10){
+//    i=i+1
+//    console.log(num,"*",i,"=",num*i)
+// }
+// // i=i+1
+
+
+// var z=require("readline-sync")
+// var i=0
+// while(i<100){
+//    i=i+1
+//    console.log(i)
+// }
+
+// var i=0
+// while(i<100){
+//    if(i%7==0){
+//       console.log(i)
+//    }
+//    i=i+1
+// }
+
+// var i=0
+// var sum=0
+// while(i<=100){
+//    sum=sum+i
+//    console.log(sum)
+//    i=i+1
+// }
+
+
+// var z=require("readline-sync")
+// var num=z.questionInt("enter num : ")
+// var i=0
+// var sum=0
+// while (i<num){
+//    var a=z.questionInt("enter num : ")
+//    sum=sum+a
+//    console.log(sum)
+//    i=i+1
+// }
+
+
+// var z=require("readline-sync")
+// var i=0
+// while (i<=100){
+//    if(i%2==0){
+//       console.log(-i)
+//    }
+//    else{
+//       console.log(i)
+//    }
+//    i=i+1
+// }
+
+// var z=require("readline-sync")
+// var i=0
+// while(i<=20){
+//    if(i%3==0){
+//       console.log(i,"nav")
+//    }
+//    else if(i%7==0){
+//       console.log(i,"gurukul")
+//    }
+//    else if(i%3==0 && i%7==0){
+//       console.log(i,"navgurukul")
+//    }
+//    i=i+1
+// }
+
+
+// var i=156
+// while(i<166){
+//    x=i-155
+//    i=i+1
+//    console.log(x)
+// }
+
+// for(var i = 0; i <= 3; i++){
+//    console.log(i)
+// }
+
+
+ 
+// for(var n=9;n>0;n--){
+//    console.log(n)
+// }
+
+
+// for (var s=25;s<=30;s++){
+//    console.log(s)
+// }
+
+// var i=1
+// do{
+//    if(i%2==0){
+//       console.log(i)
+//    }
+//    i=i+1
+// }
+// while(i<=10)
+
+
+// let i=2
+// do{
+//    console.log(i);
+// }
+// while (i >10)
+ 
+ // while(i>10){
+//    console.log(i);
+// }
+
+
+// var cars = ["Maruti", "Mercedes", "BMW"];
+// for (car in cars){
+//    console.log(cars[car])}
+
+
+// var campus_list=["Bangalore","Dharamshala","Pune","Bangalore","sarjapur"]
+// for(var campus in campus_list){
+//    console.log(campus)
+// }
+
+
+// var person={"name":"gouri","surname":"maity","age":37}
+ 
+// for (person_details in person){
+//    console.log(person_details+ " = " +person[person_details]);
+// }
+
+
+// a=["a","b","g","r","t"]
+// for(let z of a){
+// console.log(z);
+// }
+
+// const n=require("readline-sync");
+// var name=n.question("enter name: ")
+// const store=name;
+// var string=""
+// for (let i=name.length-1;i>=0;i--) {
+//   string=string+name[i]
+// }
+// if (store===string) {
+//   console.log("its palindrome string")
+// }
+// else {
+//   console.log("it's not a palindrome string")
+// }
+
+
+// var z=require("readline-sync")
+// var a=z.question("enter name : ")
+// var b=a
+// var str=""
+// var i=a.length-1
+// while(i>=0){
+//    str=str+a[i]
+// i=i-1
+// }
+// if(b===str){
+//    console.log(b,str,"palindrome")
+// }
+// else{
+//    console.log(b,str,"its not")
+// }
+
+
+// for (var i = 1; i <= 100; i++){
+//    console.log(i)
+//    };
+
+// var i=0
+// while(i<100){
+//    i=i+1
+// console.log(i)
+// }
+
+ 
+var row = 5
+while (row>=1){
+   var column=1
+   str=""
+   while(column<=5){
+       str=str+row+"";
+       column+=1;
+   }
+   console.log(str);
+   row-=1;
+}
